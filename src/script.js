5996db9260049045e9c904107b758a148473ba3b
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-05-30 16:54:08 -0400

    token exchange works, added script to .html

--- src/script.js
+++ src/script.js
@@ -52,6 +52,23 @@ async function generateCodeChallenge(codeVerifier) {
 
 async function getAccessToken(clientId, code) {
     // TODO: Get access token for code
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("code_verifier", verifier);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded"},
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
 }
 
 async function fetchProfile(token) {
