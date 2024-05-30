3cbbd20cbc8b1a41f2028066987a699406ab6ed3
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-05-30 17:29:53 -0400

    remove react code, stuck to vanilla js

--- src/script.js
+++ src/script.js
@@ -8,6 +8,7 @@ if (!code) {
 } else {
     const accessToken = await getAccessToken(clientId, code);
     const profile = await fetchProfile(accessToken);
+    console.log(profile);
     populateUI(profile);
 }
 
@@ -73,8 +74,32 @@ async function getAccessToken(clientId, code) {
 
 async function fetchProfile(token) {
     // TODO: Call Web API
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}`}
+    });
+
+    return await result.json();
+
 }
 
 function populateUI(profile) {
     // TODO: Update UI with profile data
+    // setDisplayName(profile.display_name);
+
+    document.getElementById("displayName").innerText = profile.display_name;
+    document.getElementById("id").innerText = profile.id;
+    document.getElementById("email").innerText = profile.email;
+    document.getElementById("uri").innerText = profile.uri;
+    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url").innerText = profile.href;
+    document.getElementById("url").setAttribute("href", profile.href);
+
+    if (profile.images[0]) {
+        const profileImage = new Image(600,600);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar").appendChild(profileImage);
+        document.getElementById("imgUrl").innerText = profile.images[0].url;
+    }
+
+
 }
