14baa169e370dfa24620fe544fa5e12fbd02232e
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-05-30 16:45:25 -0400

    finished function that redirects user to the Spotify auth page. (redirectToAuthCodeFLow

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 
 const clientId = "4ab54dd4daf04c39976a847c9066ce5d";
-const code = undefined;
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
 
 if (!code) {
     redirectToAuthCodeFlow(clientId);
@@ -10,8 +11,43 @@ if (!code) {
     populateUI(profile);
 }
 
-async function redirectToAuthCodeFlow(clientId) {
+export async function redirectToAuthCodeFlow(clientId) {
     // TODO: Redirect to Spotify authorization page
+
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    
+}
+
+function generateCodeVerifier(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible,length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '')
 }
 
 async function getAccessToken(clientId, code) {
