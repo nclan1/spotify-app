6b87dd9099d11052e517969d25825c29ab143d40
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-05-30 16:19:31 -0400

    started scaffold code

---
+++ src/script.js
@@ -0,0 +1,27 @@
+
+const clientId = "4ab54dd4daf04c39976a847c9066ce5d";
+const code = undefined;
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+    // TODO: Redirect to Spotify authorization page
+}
+
+async function getAccessToken(clientId, code) {
+    // TODO: Get access token for code
+}
+
+async function fetchProfile(token) {
+    // TODO: Call Web API
+}
+
+function populateUI(profile) {
+    // TODO: Update UI with profile data
+}
