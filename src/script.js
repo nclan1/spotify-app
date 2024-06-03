e6f145dbe26de58a743858a46241453dc64a46f3
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-06-03 11:54:18 -0400

    was able to store accesstoken using localstorage

--- src/script.js
+++ src/script.js
@@ -8,7 +8,8 @@ const code = params.get("code");
 if (!code) {
     redirectToAuthCodeFlow(clientId);
 } else {
-    const accessToken = await getAccessToken(clientId, code);
+    await getAccessToken(clientId, code);
+    const accessToken = localStorage.getItem("accessing_t");
     console.log(accessToken)
     const profile = await fetchProfile(accessToken);
     console.log(profile);
@@ -74,8 +75,36 @@ async function getAccessToken(clientId, code) {
 
     
     const { access_token } = await result.json();
-    return access_token;
+    console.log(access_token);
+
+    if (access_token != null) {
+        localStorage.setItem("accessing_t", access_token)
+    }
+
 }
+
+// const getRefreshToken = async () => {
+
+//     const refreshToken = localStorage.getItem('refresh_token');
+//     const url = "https://accounts.spotify.com/api/token";
+
+//     const payload = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www.form-urlencoded'
+//         },
+//         body: new URLSearchParams({
+//             grant_type: 'refresh_token',
+//             refresh_token: refreshToken,
+//             client_id: clientId
+//         }),
+//     }
+//     const body = await fetch(url, payload);
+//     const response await body.json();
+
+//     localStorage.setItem('access_token', response.accessToken);
+//     localStorage.setItem('refresh_token', response.refreshToken);
+// }
 
 async function fetchProfile(token) {
 
@@ -99,11 +128,11 @@ function populateUI(profile) {
     document.getElementById("url").innerText = profile.href;
     document.getElementById("url").setAttribute("href", profile.href);
 
-    if (profile.images[0]) {
+    if (profile.images[1]) {
         const profileImage = new Image(600,600);
-        profileImage.src = profile.images[0].url;
+        profileImage.src = profile.images[1].url;
         document.getElementById("avatar").appendChild(profileImage);
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
+        document.getElementById("imgUrl").innerText = profile.images[1].url;
     }
 
 
